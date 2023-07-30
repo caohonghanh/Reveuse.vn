04e5777f120e33c97d07a6d3e0e24b18f675ff8b
caohonghanh <140376098+caohonghanh@users.noreply.github.com>
2023-07-30 23:05:13 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+$('#sp-dress').owlCarousel({
+    loop:true,
+	autoplay: true,
+    margin:10,
+    nav:true,
+	dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-shirt').owlCarousel({
+    loop:true,
+	autoplay: true,
+    margin:10,
+    nav:true,
+	dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-pants').owlCarousel({
+    loop:true,
+	autoplay: true,
+    margin:10,
+    nav:true,
+	dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
